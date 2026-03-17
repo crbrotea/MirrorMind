3a0e2375d1f0c6360f6d2b8e336a2357aaccdd7f
Cristhian <cristhian@owqlo.com>
2026-03-17 23:24:47 +0100

    chore: initialize Flutter project structure
    - Added essential project files including pubspec.yaml, README.md, and .gitignore.
    - Configured Android and iOS directories with necessary build files and assets.
    - Established analysis options for Dart code linting.
    - Included metadata for Flutter project capabilities and dependencies.
    
    This commit sets up the foundational structure for the MirrorMind Flutter application.

---
+++ mirrormind_flutter/android/app/src/main/kotlin/com/brotea/mirrormind_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.brotea.mirrormind_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mirrormind_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_sound
+import record_darwin
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSoundPlugin.register(with: registry.registrar(forPlugin: "FlutterSoundPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
